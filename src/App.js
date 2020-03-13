707aec651710e002015023223fa83f14ea0a42a8
Kaifu Lam <kaifulam@u.washington.edu>
2020-03-12 17:59:47 -0700

    added header component and links to home, shop and contact

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  )
-};
+import Header from './components/header/header.component';
 
 function App() {
   // <Route exact path component>
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
