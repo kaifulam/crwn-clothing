3bea7f1ac0cf355be7d36c68f23381d6ad106940
Kaifu Lam <kaifulam@u.washington.edu>
2020-03-14 22:13:53 -0700

    added redux; implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           },
             () => { console.log(this.state); }
           )
         });
       }
       else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser({ userAuth });
       }
     })
   }
@@ -49,7 +45,8 @@ class App extends React.Component {
     // <Route exact path component>
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
+        {/* <Header currentUser={this.state.currentUser} /> */}
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
@@ -60,6 +57,10 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
 
 // Route provides History, Location and Match, but only pass one child in. So to pass props down to grandchildren, we have to use withRouter. Props tunneling, passing down props down the levels only to pass it on is a bad practice.
