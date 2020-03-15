50f4e7a118df2315109bb6004cae833d8edc6082
Kaifu Lam <kaifulam@u.washington.edu>
2020-03-15 04:34:06 -0700

    update application with reselect; replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
 
     </div>
 )
@@ -19,7 +20,14 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+});
+
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(CartIcon);
+
+
+//mapStateToProps will run every time the state is updated (including if say... the unrelated user states are updated). This reduce performance as React has to recompute the reduce function and regenerate the reduced number every time any state changes. Memoization, reselect library, help us cashe the reduced value so React doesn't have to regenerate the reduced value if it's not updated.
