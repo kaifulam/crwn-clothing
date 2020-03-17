94b5110011b18537b271c3cd257769cc9f912210
Kaifu Lam <kaifulam@u.washington.edu>
2020-03-16 20:30:39 -0700

    added checkout page, checkout item component and updated cart to use selectors from reselct"

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg'
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
@@ -22,17 +25,15 @@ const Header = ({ currentUser, hidden }) => (
             <Link className='option' to='/contact'>
                 CONTACT
             </Link>
-            {
-                currentUser ?
-                    <div className='option' onClick={() => auth.signOut()}>
-                        SIGN OUT
-                    </div>
-                    :
+            {currentUser ? (
+                <div className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>
+            ) : (
                     < Link className='option' to='/signin'>
                         SIGN IN
-                    </Link>
-
-            }
+                </Link>
+                )}
             <CartIcon />
         </div>
         {
@@ -41,9 +42,11 @@ const Header = ({ currentUser, hidden }) => (
     </div >
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
+
+//createSturedSelector will pass on top level state
